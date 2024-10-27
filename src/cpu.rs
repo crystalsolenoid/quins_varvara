e09779c065134e8b0154a68f32c7adb10e4b2c88
Quinten Konyn <qkonyn@gmail.com>
2024-10-26 20:51:40 -0700

    Move most opcode logic to cpu module
    The barrier to adding the rest is figuring out how to handle io writes.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,24 @@
 use super::varvara::Varvara;
 
+pub struct CodeFlags {
+    pub keep: bool,
+    pub ret: bool,
+    pub short: bool,
+}
+
+pub struct LitFlags {
+    pub ret: bool,
+    pub short: bool,
+}
+
+pub enum Code {
+    BRK,
+    DEO(CodeFlags),
+    ADD(CodeFlags),
+    SUB(CodeFlags),
+    LIT(LitFlags),
+}
+
 pub struct Stack {
     bytes: Vec::<u8>,
 }
@@ -59,7 +78,77 @@ impl Cpu {
         return u16::from_be_bytes([high_byte, low_byte])
     }
 
-//    pub fn operate(&mut varvara: Varvara) {
-//
-//    }
+    /// Do one operation
+    pub fn step(&mut self, varvara: &mut Varvara) {
+        let raw_code = self.next_byte(varvara);
+        todo!();
+    }
+
+    /// Execute ADD
+    pub fn add(&mut self, f: CodeFlags) {
+        if f.short {
+            let b = self.work.pop2();
+            let a = self.work.pop2();
+            self.work.push2(a.wrapping_add(b));
+        } else {
+            let b = self.work.pop();
+            let a = self.work.pop();
+            self.work.push(a.wrapping_add(b));
+        }
+    }
+
+    /// Execute SUB
+    pub fn sub(&mut self, f: CodeFlags) {
+        if f.short {
+            let b = self.work.pop2();
+            let a = self.work.pop2();
+            self.work.push2(a.wrapping_sub(b));
+        } else {
+            let b = self.work.pop();
+            let a = self.work.pop();
+            self.work.push(a.wrapping_sub(b));
+        }
+    }
+
+    /// Execute LIT
+    pub fn lit(&mut self, f: LitFlags, varvara: &Varvara) {
+        if f.short {
+            let short = self.next_short(varvara);
+            self.work.push2(short);
+        } else {
+            let byte = self.next_byte(varvara);
+            self.work.push(byte);
+        }
+    }
+
+    /// Execute DEO
+    pub fn deo(&mut self, f: CodeFlags) {
+        let _device = self.work.pop();
+        let value = self.work.pop();
+        todo!();
+        //out.write(&[value])?;
+        //out.flush()?;
+    }
+}
+
+pub fn parse_code(byte: u8) -> Code {
+    let code = 0b000_11111 & byte;
+    let short = 0b001_00000 & byte != 0;
+    let ret = 0b010_00000 & byte != 0;
+    if ret { todo!("Return flag not yet implemented! Code: {byte:0>2x?}"); }
+    let keep = 0b100_00000 & byte != 0;
+    if keep && code != 0x00 { todo!("Keep flag not yet implemented! Code: {byte:0>2x?}"); }
+
+    let flags = CodeFlags {keep, ret, short};
+    match code {
+        0x00 => if keep {
+                Code::LIT(LitFlags {ret, short})
+            } else {
+                Code::BRK
+            },
+        0x17 => Code::DEO(flags),
+        0x18 => Code::ADD(flags),
+        0x19 => Code::SUB(flags),
+        _ => todo!("Missing opcode! Code: {byte:0>2x?}"),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
-use uxn::cpu::Cpu;
+use uxn::cpu::{self, Cpu, CodeFlags, LitFlags, Code};
 use uxn::varvara::Varvara;
 
 fn main() -> io::Result<()> {
@@ -18,39 +18,11 @@ fn main() -> io::Result<()> {
 
     loop {
         let raw_code = uxn.next_byte(&varvara);
-        let code = parse_code(raw_code);
+        let code = cpu::parse_code(raw_code);
         match code {
-            Code::ADD(f) => {
-                if f.short {
-                    let b = uxn.work.pop2();
-                    let a = uxn.work.pop2();
-                    uxn.work.push2(a.wrapping_add(b));
-                } else {
-                    let b = uxn.work.pop();
-                    let a = uxn.work.pop();
-                    uxn.work.push(a.wrapping_add(b));
-                }
-            },
-            Code::SUB(f) => {
-                if f.short {
-                    let b = uxn.work.pop2();
-                    let a = uxn.work.pop2();
-                    uxn.work.push2(a.wrapping_sub(b));
-                } else {
-                    let b = uxn.work.pop();
-                    let a = uxn.work.pop();
-                    uxn.work.push(a.wrapping_sub(b));
-                }
-            },
-            Code::LIT(f) => {
-                if f.short {
-                    let short = uxn.next_short(&varvara);
-                    uxn.work.push2(short);
-                } else {
-                    let byte = uxn.next_byte(&varvara);
-                    uxn.work.push(byte);
-                }
-            },
+            Code::ADD(f) => uxn.add(f),
+            Code::SUB(f) => uxn.sub(f),
+            Code::LIT(f) => uxn.lit(f, &varvara),
             Code::DEO(_f) => {
                 let _device = uxn.work.pop();
                 let value = uxn.work.pop();
@@ -68,47 +40,6 @@ fn main() -> io::Result<()> {
 
 fn print_bytes(data: &[u8]) {
     println!("{:0>2x?}", &data);
-}
-
-struct CodeFlags {
-    keep: bool,
-    ret: bool,
-    short: bool,
-}
-
-struct LitFlags {
-    ret: bool,
-    short: bool,
-}
-
-enum Code {
-    BRK,
-    DEO(CodeFlags),
-    ADD(CodeFlags),
-    SUB(CodeFlags),
-    LIT(LitFlags),
-}
-
-fn parse_code(byte: u8) -> Code {
-    let code = 0b000_11111 & byte;
-    let short = 0b001_00000 & byte != 0;
-    let ret = 0b010_00000 & byte != 0;
-    if ret { todo!("Return flag not yet implemented! Code: {byte:0>2x?}"); }
-    let keep = 0b100_00000 & byte != 0;
-    if keep && code != 0x00 { todo!("Keep flag not yet implemented! Code: {byte:0>2x?}"); }
-
-    let flags = CodeFlags {keep, ret, short};
-    match code {
-        0x00 => if keep {
-                Code::LIT(LitFlags {ret, short})
-            } else {
-                Code::BRK
-            },
-        0x17 => Code::DEO(flags),
-        0x18 => Code::ADD(flags),
-        0x19 => Code::SUB(flags),
-        _ => todo!("Missing opcode! Code: {byte:0>2x?}"),
-    }
 }
 
 // They'll all implement the Varvara::Device trait
