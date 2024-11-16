8438a43912a605b2712a6b8c03b0e2ef3f10e4aa
Quinten Konyn <qkonyn@gmail.com>
2024-11-15 21:40:36 -0800

    Use new parser

--- src/tal.rs
+++ src/tal.rs
@@ -1,129 +1,18 @@
-use hex;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
+
+use crate::parse::parse_tal;
+use winnow::Parser;
 
 pub fn assemble(input: &str, output: &str) -> std::io::Result<()> {
-    let input = File::open(input)?;
-    let reader = BufReader::new(input);
+    let mut input = File::open(input)?;
 
-    let hex: Vec<u8> = reader
-        .lines()
-        .map_while(Result::ok)
-        .map(assemble_codes)
-        .flat_map(ascii_to_hex)
-        .collect();
+    let mut contents = String::new();
+    input.read_to_string(&mut contents)?;
+
+    let hex: Vec<u8> = parse_tal.parse(&contents).unwrap();
 
     std::fs::write(output, &hex)?;
 
     Ok(())
-}
-
-fn ascii_to_hex(input: String) -> Vec<u8> {
-    let mut ascii = input.as_bytes().to_owned();
-    ascii.retain(|x| !matches!(x, b' ' | b'\n'));
-
-    let chunks = ascii.chunks_exact(2);
-    if ascii.len() % 2 != 0 {
-        panic!("Odd number of hex digits. {ascii:?}");
-    }
-
-    chunks
-        .map(|a| {
-            let high = ascii_digit_to_u8(a[0]) << 4;
-            let low = ascii_digit_to_u8(a[1]);
-            high + low
-        })
-        .collect()
-}
-
-fn ascii_digit_to_u8(digit: u8) -> u8 {
-    match digit {
-        n if n.is_ascii_digit() => n - b'0',
-        n if (b'a'..=b'f').contains(&n) => 10 + n - b'a',
-        n if (b'A'..=b'F').contains(&n) => 10 + n - b'A',
-        e => panic!("Unexpected character with ASCII code: {e}"),
-    }
-}
-
-fn assemble_codes(input: String) -> String {
-    input
-        .split_ascii_whitespace()
-        .flat_map(transform_rune)
-        .map(parse_code)
-        .collect::<String>()
-}
-
-fn parse_code(token: String) -> String {
-    let token_bytes = token.as_bytes();
-
-    if (token_bytes.len() < 3) | (token_bytes.len() > 6) {
-        // assume it isn't an opcode and return unmodified
-        return token.to_string();
-    }
-
-    let (opcode, flags) = token.split_at(3);
-
-    let mut hex = match opcode {
-        "ADD" => 0x18,
-        "SUB" => 0x19,
-        "LIT" => 0x80,
-        "DEO" => 0x17,
-        "DEI" => 0x16,
-        "BRK" => 0x00,
-        "INC" => 0x01,
-        "MUL" => 0x1a,
-        "DIV" => 0x1b,
-        "SFT" => 0x1f,
-        // assume it isn't an opcode and return unmodified
-        _ => return token.to_string(),
-    };
-
-    // TODO don't let exceptions to the flag rules (like
-    // BRK and LIT) permit the hex value to get above 0xFF.
-    // Assert the order of the flags (2kr) for all present?
-    if flags.contains("k") {
-        hex += 0b100_00000;
-    }
-
-    if flags.contains("r") {
-        hex += 0b010_00000;
-    }
-
-    if flags.contains("2") {
-        hex += 0b001_00000;
-    }
-
-    hex::encode([hex])
-}
-
-fn transform_rune(token: &str) -> Vec<String> {
-    let (maybe_rune, other) = token.split_at(1);
-    match maybe_rune {
-        "%" => todo!(),
-        "|" => todo!(),
-        "$" => todo!(),
-        "@" => todo!(),
-        "&" => todo!(),
-        "#" => literal_hex(other),
-        "." => todo!(),
-        "," => todo!(),
-        ";" => todo!(),
-        ":" => todo!(),
-        "'" => todo!(),
-        "\"" => todo!(),
-        _ => vec![token.to_string()],
-    }
-}
-
-fn literal_hex(suffix: &str) -> Vec<String> {
-    let opcode = match suffix.as_bytes().len() {
-        2 => "LIT".to_string(),
-        4 => "LIT2".to_string(),
-        _ => panic!(
-            "Unexpected number of digits after a literal hex rune: {}",
-            suffix
-        ),
-    };
-    vec![opcode, suffix.to_string()]
 }
