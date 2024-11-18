fb5fbae74936fe708ada7d5366ec83882616bd4d
Quinten Konyn <qkonyn@gmail.com>
2024-11-17 23:50:53 -0800

    Replace location items with the relevant bytes

--- src/tal.rs
+++ src/tal.rs
@@ -13,14 +13,7 @@ pub fn assemble(input: &str, output: &str) -> std::io::Result<()> {
 
     let parsed: Vec<ROMItem> = parse_tal.parse(&contents).unwrap();
 
-    let hex: Vec<u8> = parsed
-        .into_iter()
-        .filter_map(|item| match item {
-            ROMItem::Location(_) => None,
-            ROMItem::Addr(_) => todo!(),
-            ROMItem::Byte(b) => Some(b),
-        })
-        .collect();
+    let hex = replace_locations(&parsed);
 
     std::fs::write(output, &hex)?;
 
@@ -46,6 +39,23 @@ fn resolve_locations<'s>(items: &'s [ROMItem]) -> HashMap<&'s str, u16> {
         .collect()
 }
 
+fn replace_locations<'s>(items: &'s [ROMItem]) -> Vec<u8> {
+    let locations = resolve_locations(&items);
+    items
+        .into_iter()
+        .filter_map(|item| match item {
+            ROMItem::Byte(b) => Some(vec![*b]),
+            ROMItem::Location(_) => None,
+            ROMItem::Addr(name) => {
+                let mut bytes = vec![0xa0];
+                bytes.extend(locations[name].to_be_bytes());
+                Some(bytes)
+            }
+        })
+        .flatten()
+        .collect()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -56,5 +66,18 @@ mod test {
         let locations = resolve_locations(&items);
         let desired = HashMap::from([("test", 0x0101)]);
         assert_eq!(locations, desired);
+    }
+
+    #[test]
+    fn replace_location() {
+        let items = vec![
+            ROMItem::Addr("arrow"),
+            ROMItem::Byte(0x00),
+            ROMItem::Location("arrow"),
+            ROMItem::Byte(0xff),
+        ];
+        let desired = vec![0xa0, 0x01, 0x04, 0x00, 0xff];
+        let replaced_items = replace_locations(&items);
+        assert_eq!(replaced_items, desired);
     }
 }
